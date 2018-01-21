4dadf631ae9fb532c24b1ac710826eecacfda3ec
Damir Vidaković <vida.damir@gmail.com>
2018-01-21 23:32:56 +0100

    saving a user

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req,res) => {
+app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
     });
@@ -21,11 +21,23 @@ app.post('/todos', (req,res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');  
+
+app.post('/users', (req, res) => {
+    var user = new User ({
+        email: req.body.email
+    });
+    
+    user.save().then((doc)=> {
+        res.send(doc);
+    },(err) => {
+        res.status(400).send(err);
+    });
 });
 
 
+app.listen(3000, () => {
+    console.log('Started on port 3000');  
+});
 
 
 
