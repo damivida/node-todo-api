f8315bcb8a518c0c2856ed955a23a6423a03b6bb
Damir Vidaković <vida.damir@gmail.com>
2018-01-31 23:32:52 +0100

    mongoose-queries

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,48 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose'); 
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+
+//var id = "5a7226320dc61af43e1d586511";
+
+
+/*if(!ObjectID.isValid(id)) {
+    console.log('ID not valide');    
+}*/
+
+/*Todo.find({
+    _id: id
+}).then((todos) => {
+    console.log('Todos', todos);
+});
+
+Todo.findOne({
+    _id: id
+}).then((todo) => {
+    console.log('Todo', todo);
+});*/
+
+
+/*Todo.findById(id).then((todo) => {
+    if (!todo) {
+        return console.log('Id not found');
+    }
+    
+     console.log('Todo', todo);
+}).catch((e) => console.log(e));*/
+
+//CHALLANGE
+
+var id = "5a610e3697244db028074fe3";
+
+User.findById(id).then((user) => {
+    if(!user) {
+        return console.log("User not found");
+    }
+    console.log(JSON.stringify(user, undefined,2));
+}, (err) => {
+    console.log(err);
+});
+
