f0f0ec98d4142ceabdda4daea4e4dff1bbd536bc
Damir Vidaković <vida.damir@gmail.com>
2018-02-16 19:34:26 +0100

    updating an todo

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -101,7 +102,37 @@ app.delete('/todos/:id', (req, res) => {
     });
 });
 
-
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    
+    //seting what user can update
+    var body = _.pick(req.body, ['text', 'completed']);
+    
+    //validating ID
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send("ID is not valid");
+    }
+    
+    //checking completed value
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completed = null;
+    }
+    
+    //upating todo
+    Todo.findByIdAndUpdate(id, {$set:body}, {new:true}).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        
+           res.send({todo});
+        }).catch((e) => {
+        res.status(400).send();
+    });
+    
+});
 
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);  
